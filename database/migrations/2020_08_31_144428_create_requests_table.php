90afbb3a6668550babea9e880afc76614631cfe8
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-31 21:56:00 +0700

    database setup, model and migration

---
+++ database/migrations/2020_08_31_144428_create_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string("uid");
+            $table->string("customer_name")->nullable();
+            $table->unsignedBigInteger('vendor_id');
+            $table->foreign("vendor_id")->references("id")->on("vendors");
+            $table->bigInteger("total");
+            $table->dateTime("deleted_at")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
---
+++ database/migrations/2020_08_31_144435_create_request_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("dishes_id");
+            $table->foreign("dishes_id")->references("id")->on("dishes");
+            $table->bigInteger("amount");
+            $table->text("information")->nullable();
+            $table->dateTime("deleted_at")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_details');
+    }
+}
